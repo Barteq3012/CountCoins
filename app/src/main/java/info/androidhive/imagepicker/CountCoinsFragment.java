b4dd824cbc384bc69a9b13c1445e377b0212e637
Barteq3012 <bartek.cz3012@gmail.com>
2021-05-07 19:24:21 +0200

    openCV library works well

--- app/src/main/java/info/androidhive/imagepicker/CountCoinsFragment.java
+++ app/src/main/java/info/androidhive/imagepicker/CountCoinsFragment.java
@@ -21,12 +21,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import java.io.IOException;
 import java.util.List;
 import info.androidhive.imagepicker.ui.main.PageViewModel;
@@ -39,18 +42,20 @@ public class CountCoinsFragment extends Fragment {
     private static final String TAG = DetectText.class.getSimpleName();
     public static final int REQUEST_IMAGE = 100;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    Button detectTextButton;
+    Button countCoinsButton;
     Button captureImageButton;
     Bitmap imageBitmap;
     TextView textView;
     ImageView selected_image;
+    Mat mat;
+    Mat grayMat;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.count_coins, container, false);
-        detectTextButton = root.findViewById(R.id.detectTextButtonId);
+        countCoinsButton = root.findViewById(R.id.countCoinsButtonId);
         captureImageButton = root.findViewById(R.id.captureImageButtonId);
         textView = root.findViewById(R.id.text_display);
         selected_image = root.findViewById(R.id.img_view);
@@ -61,11 +66,12 @@ public class CountCoinsFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        OpenCVLoader.initDebug();
 
-        detectTextButton.setOnClickListener(new View.OnClickListener() {
+        countCoinsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                detectTextFromImage();
+                countCoinsFromImage();
             }
         });
 
@@ -202,25 +208,87 @@ public class CountCoinsFragment extends Fragment {
         }
     }
 
-    private void detectTextFromImage() {
+    private void countCoinsFromImage() {
         if(imageBitmap == null){
             Toast.makeText(getActivity(), "Select a picture first!", Toast.LENGTH_SHORT).show();
             return;
         }
-        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
-        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
-        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                displayTextFromImage(firebaseVisionText);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                Log.d("Error: " , e.getMessage());
-            }
-        });
+
+        /* convert bitmap to mat */
+        mat = new Mat(imageBitmap.getWidth(), imageBitmap.getHeight(),
+                CvType.CV_8UC1);
+        grayMat = new Mat(imageBitmap.getWidth(), imageBitmap.getHeight(),
+                CvType.CV_8UC1);
+
+        Utils.bitmapToMat(imageBitmap, mat);
+
+        /* convert to grayscale */
+        int colorChannels = (mat.channels() == 3) ? Imgproc.COLOR_BGR2GRAY
+                : ((mat.channels() == 4) ? Imgproc.COLOR_BGRA2GRAY : 1);
+
+        Imgproc.cvtColor(mat, grayMat, colorChannels);
+
+        /* reduce the noise so we avoid false circle detection */
+        Imgproc.GaussianBlur(grayMat, grayMat, new Size(9, 9), 2, 2);
+
+// accumulator value
+        double dp = 1.2d;
+// minimum distance between the center coordinates of detected circles in pixels
+        double minDist = 100;
+
+// min and max radii (set these values as you desire)
+        int minRadius = 0, maxRadius = 0;
+
+// param1 = gradient value used to handle edge detection
+// param2 = Accumulator threshold value for the
+// cv2.CV_HOUGH_GRADIENT method.
+// The smaller the threshold is, the more circles will be
+// detected (including false circles).
+// The larger the threshold is, the more circles will
+// potentially be returned.
+        double param1 = 70, param2 = 72;
+
+        /* create a Mat object to store the circles detected */
+        Mat circles = new Mat(imageBitmap.getWidth(),
+                imageBitmap.getHeight(), CvType.CV_8UC1);
+
+        /* find the circle in the image */
+        Imgproc.HoughCircles(grayMat, circles,
+                Imgproc.CV_HOUGH_GRADIENT, dp, minDist, param1,
+                param2, minRadius, maxRadius);
+
+        /* get the number of circles detected */
+        int numberOfCircles = (circles.rows() == 0) ? 0 : circles.cols();
+
+        /* draw the circles found on the image */
+        for (int i=0; i<numberOfCircles; i++) {
+
+
+            /* get the circle details, circleCoordinates[0, 1, 2] = (x,y,r)
+             * (x,y) are the coordinates of the circle's center
+             */
+            double[] circleCoordinates = circles.get(0, i);
+
+
+            int x = (int) circleCoordinates[0], y = (int) circleCoordinates[1];
+
+            Point center = new Point(x, y);
+
+            int radius = (int) circleCoordinates[2];
+
+            /* circle's outline */
+            Imgproc.circle(mat, center, radius, new Scalar(0,
+                    255, 0), 4);
+
+            /* circle's center outline */
+            Imgproc.rectangle(mat, new Point(x - 5, y - 5),
+                    new Point(x + 5, y + 5),
+                    new Scalar(0, 128, 255), -1);
+        }
+
+        /* convert back to bitmap */
+        Utils.matToBitmap(mat, imageBitmap);
+        selected_image.setImageBitmap(imageBitmap);
     }
 
     private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
@@ -235,6 +303,7 @@ public class CountCoinsFragment extends Fragment {
         }
     }
 
+    // permissions
     private void showSettingsDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(getString(R.string.dialog_permission_title));
